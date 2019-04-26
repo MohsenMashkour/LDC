3ddd84ef33cc24765218dfd379bb7b3c36083f4e
Mohsen <mohsen.mashkour@gmail.com>
2019-04-26 17:34:29 +0200

    Gps class, spinner, Start button, txt view for show the data

--- app/src/main/java/com/example/ldc/MapsActivity.java
+++ app/src/main/java/com/example/ldc/MapsActivity.java
@@ -1,7 +1,18 @@
 package com.example.ldc;
 
+import android.Manifest;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,19 +21,92 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
+    Button Start;
+    TextView data;
+    Spinner spinner;
+    String[] items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        items = getResources().getStringArray(R.array.LocalizationMethods);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+       /* spinner = (Spinner) findViewById(R.id.spinner);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String lMtd = spinner.getSelectedItem().toString();
+
+                Toast.makeText(getApplicationContext(),lMtd, Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });*/
+
+        Start = (Button) findViewById(R.id.button);
+        ActivityCompat.requestPermissions(MapsActivity.this,  new String[]{Manifest.permission.ACCESS_FINE_LOCATION},876);
+
+        Start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String lMtd = spinner.getSelectedItem().toString();
+               // String i = lMtd;
+
+                switch (lMtd){
+                    case "GPS":
+                        TrackingGps trackingGps = new TrackingGps(getApplicationContext());
+                        Location location = trackingGps.getLocation();
+                        if(location != null){
+                            data = (TextView) findViewById(R.id.textView);
+                            data.setText(location.toString());
+                            double lat = location.getLatitude();
+                            double lon = location.getLongitude();
+                            Toast.makeText(getApplicationContext(),"LAT: "+ lat +" \n LON: " + lon, Toast.LENGTH_LONG).show();
+
+                        }
+                        break;
+
+                    case "Cellular":
+                        Toast.makeText(getApplicationContext(),lMtd + " selected", Toast.LENGTH_SHORT).show();
+
+                        break;
+
+
+                    case "Cell+WiFi":
+                        Toast.makeText(getApplicationContext(),lMtd + " selected", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+
+
+
+
+            }
+        });
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+
 
 
     /**
@@ -37,10 +121,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        TrackingGps trackingGps = new TrackingGps(getApplicationContext());
+        Location location = trackingGps.getLocation();
+        if(location != null){
+            double lat = location.getLatitude();
+            double lon = location.getLongitude();
+            LatLng latLng = new LatLng(lat, lon);
+            mMap.addMarker(new MarkerOptions().position(latLng).title("your current position"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
     }
 }
